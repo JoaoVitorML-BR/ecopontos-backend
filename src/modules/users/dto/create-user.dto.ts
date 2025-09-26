32d475589023c9ea565aacd042259f17534d11ac
JoaoVitorML-BR <jvmlira33@gmail.com>
2025-09-26 10:40:28 -0300

    refactor: ajustado dto para verificação de senha

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,21 +1,16 @@
-import { IsString, IsNotEmpty, MinLength, IsEmail, IsOptional, IsEnum } from 'class-validator';
-import { UserRole } from '../schemas/user.schema';
+import { IsString, IsNotEmpty, MinLength, MaxLength, Matches } from 'class-validator';
 
 export class CreateUserDto {
   @IsString({ message: 'Nome deve ser uma string' })
   @IsNotEmpty({ message: 'Nome é obrigatório' })
   name: string;
 
-  @IsEmail({}, { message: 'Email deve ter um formato válido' })
-  @IsNotEmpty({ message: 'Email é obrigatório' })
-  email: string;
-
   @IsString({ message: 'Senha deve ser uma string' })
   @IsNotEmpty({ message: 'Senha é obrigatória' })
   @MinLength(6, { message: 'Senha deve ter pelo menos 6 caracteres' })
+  @MaxLength(25, { message: 'Senha deve ter no máximo 25 caracteres' })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z0-9]).+$/, {
+    message: 'A senha deve conter pelo menos 1 letra maiúscula, 1 minúscula, 1 número e 1 caractere especial',
+  })
   password: string;
-
-  @IsOptional()
-  @IsEnum(UserRole, { message: 'Role deve ser admin ou user' })
-  role?: UserRole;
 }
